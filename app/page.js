ff2b5dbefe556c05775ce2a44052532ecbf5147b
cleonenas123-dev <cleonenas123@gmail.com>
2026-01-27 19:20:20 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ background:"#fff", border:"1px solid #e5e7eb", borderRadius:14, padding:16 }}>
+      <h1>Alerta de Licitação</h1>
+      <p>App no ar ✅</p>
+    </div>
+  );
+}
